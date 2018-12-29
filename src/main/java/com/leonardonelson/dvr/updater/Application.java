9918756124876073061f2473a2ce7b6c1d8a7c7a
Leonardo Nelson <leonardosn9@gmail.com>
2018-12-29 21:52:04 -0200

    Added linux support and included gradle wrapper

--- src/main/java/com/leonardonelson/dvr/updater/Application.java
+++ src/main/java/com/leonardonelson/dvr/updater/Application.java
@@ -15,7 +15,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableScheduling
 public class Application {
 
-    private WebDriver driver;
+    private static final String OS = System.getProperty("os.name").toLowerCase();
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class);
@@ -23,11 +23,14 @@ public class Application {
 
     @Bean(destroyMethod = "quit")
     public WebDriver webDriver() {
+        final String driverOS = isWindows() ? "windows" : "linux";
+        final String extension = isWindows() ? ".exe" : "";
         System.setProperty("webdriver.gecko.driver",
-                Application.class.getResource("/drivers/geckodriver.exe").getPath());
+                            Application.class.getResource(
+                                    "/drivers/" + driverOS + "/geckodriver" + extension).getPath());
         FirefoxOptions options = new FirefoxOptions();
         options.setHeadless(true);
-        driver = new FirefoxDriver(options);
+        final WebDriver driver = new FirefoxDriver(options);
         driver.manage().window().setSize(new Dimension(1920,1080));
         driver.manage().window().maximize();
         return driver;
@@ -37,4 +40,9 @@ public class Application {
     public RestOperations restOperations() {
        return new RestTemplate();
     }
+
+    public static boolean isWindows() {
+        return (OS.indexOf("win") >= 0);
+    }
+
 }
