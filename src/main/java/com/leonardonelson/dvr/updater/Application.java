e57ca7cef994ba8a62187e134beaa4bba7b61423
Leonardo Nelson <leonardosn9@gmail.com>
2018-12-31 20:49:46 -0200

    Introduced PropertiesPlaceholderConfigurer Bean in order to listen to unix-styled environment variables

--- src/main/java/com/leonardonelson/dvr/updater/Application.java
+++ src/main/java/com/leonardonelson/dvr/updater/Application.java
@@ -4,6 +4,7 @@ import com.gargoylesoftware.htmlunit.WebClient;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.support.PropertySourcesPlaceholderConfigurer;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestOperations;
 import org.springframework.web.client.RestTemplate;
@@ -24,6 +25,11 @@ public class Application {
     @Bean
     public RestOperations restOperations() {
        return new RestTemplate();
+    }
+
+    @Bean
+    public static PropertySourcesPlaceholderConfigurer propertySourcesPlaceholderConfigurer() {
+        return new PropertySourcesPlaceholderConfigurer();
     }
 
 }
